e0301fc2add06ae009dd3595760dc51c735b7b7d
pcapriles <pedro.capriles@walmart.com>
2021-04-30 14:28:53 -0400

    Introducing React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const PageTwo = () => {
+  return <div>PageTwo
+      <button>Click Me!</button>
+  </div>;
+};
 
 const App = () => {
-    return <div>App</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" exact component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
