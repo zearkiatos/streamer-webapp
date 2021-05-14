d273a88905872a53f80651cd73b43d8c9e88c6a6
zearkiatos <caprilespe@outlook.com>
2021-05-14 16:21:09 -0400

    Handling Auth Status Through Redux

--- src/components/googleAuth/index.js
+++ src/components/googleAuth/index.js
@@ -4,7 +4,6 @@ import { signIn, signOut } from "../../actions";
 import config from "../../config";
 
 class GoogleAuth extends React.Component {
-  state = { isSignedIn: null };
 
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
@@ -15,7 +14,7 @@ class GoogleAuth extends React.Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
@@ -35,9 +34,9 @@ class GoogleAuth extends React.Component {
   };
 
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
@@ -59,4 +58,8 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default connect(null, { signIn, signOut })(GoogleAuth);
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
