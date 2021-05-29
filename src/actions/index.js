a4ceaa8d8f82b936b3a89de0a6579f31118387c4
zearkiatos <caprilespe@outlook.com>
2021-05-28 21:52:25 -0400

    Update CI/CD config file for deploy application and Bulk action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,3 +18,27 @@ export const createStream = formValues => async dispatch => {
 
   dispatch({ type: TYPES.CREATE_STREAM, payload: response.data });
 };
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams');
+
+  dispatch({ type: TYPES.FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id) => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: TYPES.FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: TYPES.EDIT_STREAM, payload: response.data })
+};
+
+export const deleteStream = (id) => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: TYPES.DELETE_STREAM });
+};
