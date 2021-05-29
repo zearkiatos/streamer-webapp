2d2cfe93528fef1b905e4788d621c75a1251303e
zearkiatos <caprilespe@outlook.com>
2021-05-29 09:45:28 -0400

    Merging Lists of Records

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,15 +1,17 @@
-import { omit } from 'lodash';
+import { omit, mapKeys } from 'lodash';
 import TYPES from '../types';
 
 const streamReducer = (state = {}, action) => {
     switch (action.types) {
-        case TYPES.FETCH_STREAM:
-            return { ...state, [action.payload.id]: action.payload };
         case TYPES.FETCH_STREAMS:
+            return { ...state, ...mapKeys(action.payload, 'id') };
+        case TYPES.FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case TYPES.CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case TYPES.EDIT_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case TYPES.DELETE_STREAM:
             return omit(state, action.payload);
         default:
             return state;
