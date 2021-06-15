3ea81d1853c74e564a77075e72e1b0de9f553bde
pcapriles <pedro.capriles@walmart.com>
2021-06-15 12:13:49 -0400

    Making the Modal Reusable

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -10,14 +10,13 @@ const Modal = props => {
         >
             <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
                 <div className="header">
-                    Delete Stream
+                    {props.title}
                 </div>
                 <div className="content">
-                    Are you sure you want to delete this stream? 🤔
+                    {props.content}
                 </div>
                 <div className="actions">
-                    <button className="ui primary button negative">Delete</button>
-                    <button className="ui button">Cancel</button>
+                    {props.actions}
                 </div>
 
             </div>
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Modal from '../modal'
 const StreamDelete = () => {
+    const actions = (
+        <div>
+            <button className="ui primary button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </div>
+    );
     return (
         <div>
             StreamDelete
-            <Modal />
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream? 🤔"
+                actions={actions}
+            />
         </div>
     );
 };
