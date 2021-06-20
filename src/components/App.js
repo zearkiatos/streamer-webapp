fb0b0bce752c7c8e1aa48d39ac92f198bda10459
zearkiatos <caprilespe@outlook.com>
2021-06-20 10:30:15 -0400

    Viewing a Stream

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ const App = () => {
           <Route path="/streams/new" exact component={StreamCreate}></Route>
           <Route path="/streams/edit/:id" exact component={StreamEdit}></Route>
           <Route path="/streams/delete/:id" exact component={StreamDelete}></Route>
-          <Route path="/streams/show" exact component={StreamShow}></Route>
+          <Route path="/streams/:id" exact component={StreamShow}></Route>
         </div>
       </Router>
     </div>
