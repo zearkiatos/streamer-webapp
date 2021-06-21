1f1c12376ab72f6e6d9d041ef5838142b2604d52
zearkiatos <caprilespe@outlook.com>
2021-06-21 09:50:59 -0400

    Switches with React-Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -14,11 +14,13 @@ const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList}></Route>
-          <Route path="/streams/new" exact component={StreamCreate}></Route>
-          <Route path="/streams/edit/:id" exact component={StreamEdit}></Route>
-          <Route path="/streams/delete/:id" exact component={StreamDelete}></Route>
-          <Route path="/streams/show/:id" exact component={StreamShow}></Route>
+          <Switch>
+            <Route path="/" exact component={StreamList}></Route>
+            <Route path="/streams/new" exact component={StreamCreate}></Route>
+            <Route path="/streams/edit/:id" exact component={StreamEdit}></Route>
+            <Route path="/streams/delete/:id" exact component={StreamDelete}></Route>
+            <Route path="/streams/:id" exact component={StreamShow}></Route>
+          </Switch>
         </div>
       </Router>
     </div>
