dfa1b17fda77152ef8883cf16ab066834fa553a3
zearkiatos <caprilespe@outlook.com>
2021-06-26 17:08:37 -0400

    It Works

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,9 @@
 if [ $(echo "$ENVIRONMENT")=="develop" ]
 then
     unset REACT_APP_STREAMS_API_BASE_URL
+    unset REACT_APP_RTMP_SERVER_BASE_URL
     export REACT_APP_STREAMS_API_BASE_URL=http://localhost:5001/
+    export REACT_APP_RTMP_SERVER_BASE_URL=http://localhost:9000/
     npm run dev
 else
     npm run build
